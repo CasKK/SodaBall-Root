e92e92fb560812e9bddc019b08355a09c2aa4698
CasperKK <casper.siem@gmail.com>
2026-03-20 17:45:03 +0100

    fixes

--- SodaBall_Python/SodaBall3.py
+++ SodaBall_Python/SodaBall3.py
@@ -1034,44 +1034,30 @@ while running:
 
     if current_money_1 != last_money_1 or reset1:
         background_surface1.blit(money_cover, (2 * SCALE_FACTOR, 0))
-        background_surface2.blit(money_cover,
-                                 (BASE_WIDTH - money_cover.get_width() - 2 * SCALE_FACTOR, 0))
-        background_surface1.blit(wind_img1, (int(BASE_WIDTH / 12), -10 * SCALE_FACTOR))
-        background_surface2.blit(wind_img,
-                                 (int(BASE_WIDTH - BASE_WIDTH / 12 - wind_img.get_width()),
-                                  -10 * SCALE_FACTOR))
+        background_surface2.blit(money_cover, (BASE_WIDTH - money_cover.get_width() - 2 * SCALE_FACTOR, 0))
+        background_surface1.blit(wind_img1, (40*SCALE_FACTOR, -10 * SCALE_FACTOR))
+        background_surface2.blit(wind_img, (BASE_WIDTH - 40 * SCALE_FACTOR - wind_img.get_width(), -10 * SCALE_FACTOR))
         money_text_1 = font.render(f"{int(controller.money[1] / 20)}", True, RED)
-        background_surface1.blit(money_text_1, (int(BASE_WIDTH / 50), 8 * SCALE_FACTOR))
-        background_surface2.blit(money_text_1,
-                                 (int(BASE_WIDTH - (BASE_WIDTH / 50 + money_text_1.get_width())),
-                                  8 * SCALE_FACTOR))
+        background_surface1.blit(money_text_1, (10 * SCALE_FACTOR, 8 * SCALE_FACTOR))
+        background_surface2.blit(money_text_1, (BASE_WIDTH - (10 * SCALE_FACTOR + money_text_1.get_width()), 8 * SCALE_FACTOR))
         if current_money_1 % 20 != 0:
             money_text_1_1 = fontSmall.render(f"5", True, RED)
-            background_surface1.blit(money_text_1_1, (int(BASE_WIDTH / 30), 20 * SCALE_FACTOR))
-            background_surface2.blit(money_text_1_1,
-                                    (int(BASE_WIDTH - (BASE_WIDTH / 55 + money_text_1.get_width())),
-                                    20 * SCALE_FACTOR))
+            background_surface1.blit(money_text_1_1, (20 + money_text_1.get_width() - 20, 20 * SCALE_FACTOR))
+            background_surface2.blit(money_text_1_1, (BASE_WIDTH - (10 * SCALE_FACTOR + money_text_1.get_width()), 20 * SCALE_FACTOR))
         last_money_1 = current_money_1
 
     if current_money_2 != last_money_2 or reset1:
         background_surface2.blit(money_cover, (2 * SCALE_FACTOR, 0))
-        background_surface1.blit(money_cover,
-                                 (BASE_WIDTH - money_cover.get_width() - 2 * SCALE_FACTOR, 0))
-        background_surface2.blit(wind_img1, (int(BASE_WIDTH / 12), -10 * SCALE_FACTOR))
-        background_surface1.blit(wind_img,
-                                 (int(BASE_WIDTH - BASE_WIDTH / 12 - wind_img.get_width()),
-                                  -10 * SCALE_FACTOR))
+        background_surface1.blit(money_cover, (BASE_WIDTH - money_cover.get_width() - 2 * SCALE_FACTOR, 0))
+        background_surface2.blit(wind_img1, (40 * SCALE_FACTOR, -10 * SCALE_FACTOR))
+        background_surface1.blit(wind_img, (BASE_WIDTH - 40 * SCALE_FACTOR - wind_img.get_width(), -10 * SCALE_FACTOR))
         money_text_2 = font.render(f"{int(controller.money[2] / 20)}", True, RED)
-        background_surface1.blit(money_text_2,
-                                 (int(BASE_WIDTH - (BASE_WIDTH / 50 + money_text_2.get_width())),
-                                  8 * SCALE_FACTOR))
-        background_surface2.blit(money_text_2, (int(BASE_WIDTH / 50), 8 * SCALE_FACTOR))
-        if current_money_1 % 20 != 0:
+        background_surface1.blit(money_text_2, (BASE_WIDTH - (10 * SCALE_FACTOR + money_text_2.get_width()), 8 * SCALE_FACTOR))
+        background_surface2.blit(money_text_2, (10 * SCALE_FACTOR, 8 * SCALE_FACTOR))
+        if current_money_2 % 20 != 0:
             money_text_2_1 = fontSmall.render(f"5", True, RED)
-            background_surface1.blit(money_text_2_1,
-                                 (int(BASE_WIDTH - (BASE_WIDTH / 30 + money_text_2.get_width())),
-                                  20 * SCALE_FACTOR))
-            background_surface2.blit(money_text_2_1, (int(BASE_WIDTH / 55), 20 * SCALE_FACTOR))
+            background_surface1.blit(money_text_2_1, (BASE_WIDTH - money_text_2.get_width() - 20, 20 * SCALE_FACTOR))
+            background_surface2.blit(money_text_2_1, (20, 20 * SCALE_FACTOR))
         last_money_2 = current_money_2
 
     # Score
